5e8fa6d7be201e8df574e5b0c3dd173026327787
gengyue <gengyue@minigui.org>
2021-04-25 11:33:42 +0800

    modified license

--- include/sysconfig.h
+++ include/sysconfig.h
@@ -10,54 +10,23 @@
 // https://www.fmsoft.cn/exception-list
 //
 //////////////////////////////////////////////////////////////////////////////
-/**
- * \file sysconfig.h
- * \author Gengyue <gengyue@minigui.org>
- * \date 2020/09/16
- *
- * \brief This file includes some interfaces used by Hybrid OS.
- *
- \verbatim
-
-    This file is part of hishell, a developing operating system based on
-    MiniGUI. HybridOs will support embedded systems and smart IoT devices.
-
-    Copyright (C) 2002~2020, Beijing FMSoft Technologies Co., Ltd.
-    Copyright (C) 1998~2002, WEI Yongming
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Or,
-
-    As this program is a library, any link to this program must follow
-    GNU General Public License version 3 (GPLv3). If you cannot accept
-    GPLv3, you need to be licensed from FMSoft.
-
-    If you have got a commercial license of this program, please use it
-    under the terms and conditions of the commercial license.
-
-    For more information about the commercial license, please refer to
-    <http://www.minigui.com/blog/minigui-licensing-policy/>.
-
- \endverbatim
- */
-
 /*
- * $Id: sysconfig.h 13674 2020-09-16 06:45:01Z Gengyue $
- *
- *      HybridOS for Linux, VxWorks, NuCleus, OSE.
- */
+** sysconfig.h: It defines some strutures and micro for all applications.
+**
+** Copyright (C) 2021 FMSoft (http://www.fmsoft.cn).
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
 
 #ifndef _SYSTEM_CONFIG
 #define _SYSTEM_CONFIG
@@ -69,30 +38,30 @@
 #define HIBUS_PROCEDURE_LAUNCHRUNNER    "launchRunner"
 
 // Customer Require Id
-#define FORMAT_REQID                (MAX_SYS_REQID + 1)
+#define FORMAT_REQID                    (MAX_SYS_REQID + 1)
 
 // Customer subscribe require Id
-#define REQ_SUBMIT_PID_HWND         0   // inform mginit, the pid and main hwnd of hijs
+#define REQ_SUBMIT_PID_HWND             0   // inform mginit, the pid and main hwnd of hijs
 
 // user define message
-#define MSG_MAINWINDOW_CHANGE       (MSG_USER + 1)
-#define MSG_CONFIG_CHANGE           (MSG_USER + 2)
+#define MSG_MAINWINDOW_CHANGE           (MSG_USER + 1)
+#define MSG_CONFIG_CHANGE               (MSG_USER + 2)
 
 // configuration file
-#define SYSTEM_CONFIG_FILE  "hybridos.cfg"  // configuration file
+#define SYSTEM_CONFIG_FILE              "hybridos.cfg"  // configuration file
 
 
 typedef struct tagRequestInfo 
 {
-    int id;                     // sub request ID
-    HWND hwnd;                  // the window handle of the sending window
-    pid_t pid;                  // the pid of process
-    int znode;                  // znode index of bar
+    int id;                             // sub request ID
+    HWND hwnd;                          // the window handle of the sending window
+    pid_t pid;                          // the pid of process
+    int znode;                          // znode index of bar
 } RequestInfo;
 
 typedef struct tagReplyInfo 
 {
-    int id;                     // sub request ID
+    int id;                             // sub request ID
     unsigned int iData0;
     unsigned int request1;
     unsigned int request2;
